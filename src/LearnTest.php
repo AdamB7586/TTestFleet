b7069c84ef3aa263218f7712108d773761b5c7f2
Adam Binnersley <abinnersley@gmail.com>
2017-09-01 16:12:29 +0100

    Add test variation

--- src/LearnTest.php
+++ src/LearnTest.php
@@ -3,5 +3,177 @@
 namespace TheoryTest\Fleet;
 
 class LearnTest extends \TheoryTest\Car\LearnTest{
+    public $questionsTable = 'fleet_questions';
+    public $progressTable = 'fleet_progress';
+    public $dsaCategoriesTable = 'fleet_sections';
     
+    public function __construct(Database $db, Smarty $layout, User $user, $userID = false) {
+        parent::__construct($db, $layout, $user, $userID);
+        $this->setImagePath(ROOT.DS.'images'.DS.'fleet'.DS);
+    }
+    
+    /**
+     * Creates a new test for the 
+     * @param int $sectionNo This should be the section number for the test
+     */
+    public function createNewTest($sectionNo = '1'){
+        $this->clearSettings();
+        $this->chooseQuestions($sectionNo);
+        $this->setTest($sectionNo);
+        $learnName = self::$db->select('fleet_sections', array('section' => $sectionNo), array('name', 'free'));
+        if($learnName['free'] == 0){self::$user->checkUserAccess(NULL, 'fleet');}
+        $this->setTestName($learnName['name']);
+        return $this->buildTest();
+    }
+    
+    /**
+     * Gets the questions for the current section test
+     * @param int $sectionNo This should be the section number for the test
+     */
+    protected function chooseQuestions($sectionNo) {
+        $this->testInfo['section'] = $sectionNo;
+        setcookie('testinfo', serialize($this->testInfo), time() + 31536000, '/');
+    }
+    
+    /**
+     * Returns the question data for the given prim number
+     * @param int $prim Should be the question prim number
+     * @return array|boolean Returns question data as array if data exists else returns false
+     */
+    protected function getQuestionData($prim){
+        return self::$db->select($this->questionsTable, array('prim' => $prim), array('prim', 'question', 'mark', 'option1', 'option2', 'option3', 'option4', 'answerletters', 'format', 'dsaimageid'));
+    }
+    
+    /**
+     * Make sure the audio doesn't appear as no audio currently exists for the fleet questions
+     * @return boolean Returns false as no fleet audio exists
+     */
+    protected function audioButton(){
+        return false;
+    }
+    
+    /**
+     * Returns the HTML5 audio information as a string
+     * @param int $prim This should be the question prim number
+     * @param string $letter This should be the letter of the question or answer
+     * @return boolean Returns false as no audio exists for fleet
+     */
+    protected function addAudio($prim, $letter){
+        return false;
+    }
+    
+    /**
+     * Returns the number of questions in the current section
+     * @return int This should be the number of questions for the section
+     */
+    public function numQuestions(){
+        return count(self::$db->selectAll($this->questionsTable, array('dsacat' => $this->testInfo['section']), array('prim')));
+    }
+    
+    /**
+     * Returns the current question number
+     * @param int $prim This should be the current questions unique prim number
+     * @return int Returns the current question number
+     */
+    protected function currentQuestion(){
+        if(!isset($this->current)){
+            $currentnum = self::$db->select($this->questionsTable, array('prim' => $this->currentPrim, 'dsacat' => $this->testInfo['section']), array('dsaqposition'));
+            $this->current = $currentnum['dsaqposition'];
+        }
+        return $this->current;
+    }
+    
+    /**
+     * Returns the Previous question HTML for the current question
+     * @return string Returns the previous question HTML with the correct prim number for the previous question
+     */
+    protected function prevQuestion(){
+        if($_COOKIE['skipCorrect'] == 1){$prim = $this->getIncomplete('prev');}
+        elseif($this->currentQuestion() != 1){
+            $prim = self::$db->select($this->questionsTable, array('dsaqposition' => array('<', $this->currentQuestion()), 'dsacat' => $this->testInfo['section']), array('prim'), 0, array('dsaqposition' => 'DESC'));
+        }
+        else{$prim = $this->getLastQuestion();}
+        return '<div class="prevquestion btn btn-theory" id="'.$prim.'"><span class="fa fa-angle-left fa-fw"></span><span class="hidden-xs"> Previous</span></div>';
+    }
+    
+    /**
+     * Returns the Next question HTML for the current question
+     * @return string Returns the next question HTML with the correct prim number for the next question
+     */
+    protected function nextQuestion(){
+        if($_COOKIE['skipCorrect'] == 1){$prim = $this->getIncomplete();}
+        elseif($this->currentQuestion() < $this->numQuestions()){
+            $prim = self::$db->fetchColumn($this->questionsTable, array('dsaqposition' => array('>', $this->currentQuestion()), 'dsacat' => $this->testInfo['section']), array('prim'), 0, array('dsaqposition' => 'ASC'));
+        }
+        else{$prim = $this->getFirstQuestion();}
+        return '<div class="nextquestion btn btn-theory" id="'.$prim.'"><span class="hidden-xs">Next </span><span class="fa fa-angle-right fa-fw"></span></div>';
+    }
+    
+    /**
+     * Returns the prim number for the next or previous incomplete question
+     * @param string $nextOrPrev Should be either next of previous for which way you want the next question to be
+     * @return int|string Returns the prim number for the next or previous question or none if no more incomplete questions exist
+     */
+    protected function getIncomplete($nextOrPrev = 'next'){
+        if(strtolower($nextOrPrev) == 'next'){$dir = '>'; $sort = 'ASC'; $start = '0';}
+        else{$dir = '<'; $sort = 'DESC'; $start = '100000';}
+        
+        $questions = self::$db->selectAll($this->questionsTable, array('dsaqposition' => array($dir, $this->currentQuestion()), 'dsacat' => $this->testInfo['section']), array('prim'), array('dsaqposition' => $sort));
+        foreach($questions as $question){
+            if($this->useranswers[$question['prim']]['status'] <= 1){
+                return $question['prim'];
+            }
+        }
+        
+        $questions = self::$db->selectAll($this->questionsTable, array('dsaqposition' => array($dir, $start), 'dsacat' => $this->testInfo['section']), array('prim'), array('dsaqposition' => $sort));
+        foreach($questions as $question){
+            if($this->useranswers[$question['prim']]['status'] <= 1){
+                return $question['prim'];
+            }
+        }
+        return 'none';
+    }
+    
+    /**
+     * Returns the first questions prim number for the current section
+     * @return int Returns the prim number of the first question in the current section
+     */
+    protected function getFirstQuestion(){
+        return self::$db->fetchColumn($this->questionsTable, array('dsaqposition' => '1', 'dsacat' => $this->testInfo['section']), array('prim'));
+    }
+    
+    /**
+     * Returns the prim number for the last question
+     * @return int Returns the prim number for the last question
+     */
+    protected function getLastQuestion(){
+        return self::$db->fetchColumn($this->questionsTable, array('dsacat' => $this->testInfo['section']), array('prim'), array('dsaqposition' => 'DESC'));
+    }
+    
+    /**
+     * Returns any extra content to be displayed on the page
+     * @return string
+     */
+    protected function extraContent(){
+        if($_COOKIE['skipCorrect'] == 1){$skipcorrect = ' flagged';}
+        return '</div></div><div class="row"><div><div class="col-xs-12 skipcorrectclear"><div class="skipcorrect btn btn-theory'.$skipcorrect.'">Skip Correct</div></div>';
+    }
+    
+    /**
+     * Returns the correct button for the learning test section
+     * @return string Returns the button HTML
+     */
+    protected function flagHintButton(){
+        return false;
+    }
+    
+    /**
+     * Returns any related information about the current question
+     * @param string $explanation This should be the DSA explanation for the database as it has already been retrieved
+     * @param int $prim This should be the questions unique prim number
+     * @return boolean Should only return false as no fleet explanations exist
+     */
+    public function dsaExplanation($explanation, $prim){
+        return false;
+    }
 }
--- src/TheoryTest.php
+++ src/TheoryTest.php
@@ -3,5 +3,263 @@
 namespace TheoryTest\Fleet;
 
 class TheoryTest extends \TheoryTest\Car\TheoryTest{
+    protected $testNo;
+    protected $seconds = 5400;
+    protected $section = 'aditheory';
     
+    protected $testName = 'Fleet Theory Test';
+    
+    public $passmark = 85;
+    public $passmarkPerCat = 20;
+    
+    public $questionsTable = 'fleet_questions';
+    public $progressTable = 'fleet_test_progress';
+    public $dsaCategoriesTable = 'fleet_sections';
+    
+    protected $testType = 'fleet';
+    
+    public function __construct(Database $db, Smarty $layout, User $user, $userID = false) {
+        parent::__construct($db, $layout, $user, $userID);
+        $this->setImagePath(ROOT.DS.'images'.DS.'fleet'.DS);
+    }
+
+    /**
+     * Create a new Random Fleet Theory Test for the test number given
+     * @param int $theorytest Should be the test number
+     */
+    public function createNewTest(){
+        $this->clearSettings();
+        self::$user->checkUserAccess(100, 'fleet');
+        if($this->anyExisting() === false){
+            $this->chooseQuestions(1);
+            $testInfo = self::$db->select($this->progressTable, array('user_id' => $this->getUserID(), 'type' => $this->getTestType(), 'status' => 0));
+            $this->setTest($testInfo['id']);
+        }
+        return $this->buildTest();
+    }
+    
+    /**
+     * Choose some random questions from each of the categories and insert them into the progress database
+     * @param int $testNo This should be the test number you which to get the questions for
+     * @return boolean
+     */
+    protected function chooseQuestions($testNo){
+        self::$db->delete($this->progressTable, array('user_id' => $this->getUserID(), 'type' => $this->getTestType(), 'status' => 0));
+        $questions = self::$db->query("(SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '1' LIMIT 25)
+UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '2' LIMIT 25)
+UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '3' LIMIT 25)
+UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '4' LIMIT 25) ORDER BY RAND();");
+        unset($_SESSION['test'.$this->getTest()]);
+        foreach($questions as $q => $question){
+            $this->questions[($q + 1)] = $question['prim'];
+        }
+        return self::$db->insert($this->progressTable, array('user_id' => $this->getUserID(), 'questions' => serialize($this->questions), 'answers' => serialize(array()), 'test_id' => $this->testNo, 'started' => date('Y-m-d H:i:s'), 'status' => 0, 'type' => $this->getTestType()));
+    }
+    
+    /**
+     * Checks to see if their is currently a test which is not complete or a test which has already been passed
+     * @return string|boolean
+     */
+    protected function anyExisting(){
+        $existing = self::$db->select($this->progressTable, array('user_id' => $this->getUserID(), 'type' => $this->getTestType(), 'status' => array('<=', 1)));
+        if($existing){
+            $this->exists = true;
+            if($existing['status'] == 1){return 'passed';}
+            else{return 'exists';}
+        }
+        return false;
+    }
+    
+    /**
+     * Gets the questions array from the database if $this->questions is not set
+     * @return array Returns the questions array
+     */
+    public function getQuestions(){
+        if(!isset($this->questions)){
+            $questions = self::$db->fetchColumn($this->progressTable, array('user_id' => $this->getUserID(), 'id' => $this->getTest(), 'type' => $this->getTestType()), array('questions'), 0, array('started' => 'DESC'));
+            if($questions){
+                $this->questions = unserialize($questions);
+                return $this->questions;
+            }
+            return false;
+        }
+    }
+    
+    /**
+     * Returns the current users answers for the current test
+     * @return array Returns the current users answers for the current test
+     */
+    public function getUserAnswers(){
+        if(!isset($this->useranswers)){
+            $answers = self::$db->select($this->progressTable, array('user_id' => $this->getUserID(), 'id' => $this->getTest(), 'type' => $this->getTestType()), array('id', 'answers', 'question_no'), array('started' => 'DESC'));
+            if($answers){
+                $this->useranswers = unserialize($answers['answers']);
+                if(!isset($_SESSION['test'.$this->getTest()])){$_SESSION['test'.$this->getTest()] = $this->useranswers;}
+                if(!isset($_SESSION['question_no']['test'.$this->getTest()])){$_SESSION['question_no']['test'.$this->getTest()] = $answers['question_no'];}
+                $this->testID = $answers['id'];
+                return $this->useranswers;
+            }
+            return false;
+        }
+    }
+    
+    /**
+     * Updates the useranswers field in the progress table in the database
+     * @return boolean
+     */
+    protected function updateAnswers(){
+        return self::$db->update($this->progressTable, array('answers' => serialize($_SESSION['test'.$this->getTest()]), 'time_remaining' => $_SESSION['time_remaining']['test'.$this->getTest()], 'question_no' => $_SESSION['question_no']['test'.$this->getTest()]), array('user_id' => $this->getUserID(), 'id' => $this->getTest()));
+    }
+    
+    /**
+     * Returns the question data for the given prim number
+     * @param int $prim Should be the question prim number
+     * @return array|boolean Returns question data as array if data exists else returns false
+     */
+    protected function getQuestionData($prim){
+        return  self::$db->select($this->questionsTable, array('prim' => $prim), array('prim', 'question', 'mark', 'option1', 'option2', 'option3', 'option4', 'answerletters', 'format', 'dsaimageid'));
+    }
+    
+    /**
+     * Make sure the audio doesn't appear as no audio currently exists for the fleet questions
+     * @return boolean Returns false as no fleet audio exists
+     */
+    protected function audioButton(){
+        return false;
+    }
+    
+    /**
+     * Returns the HTML5 audio HTML information as a string
+     * @param int $prim This should be the question prim number
+     * @param string $letter This should be the letter of the question or answer
+     * @return boolean Returns false as no audio exists for fleet
+     */
+    protected function addAudio($prim, $letter){
+        return false;
+    }
+   
+    /**
+     * Returns the correct HTML for the DSA explanation in the review section
+     * @param string $explanation Should be the DSA explanation for the particular question
+     * @param int $prim Should be the prim number of the current question
+     * @return string|boolean Returns the HTML string if in the review section else returns false
+     */
+    public function dsaExplanation($explanation, $prim){
+        return false;
+    }
+    
+    /**
+     * Returns the flag hint button if it should be displayed
+     * @param int $prim The prim number of the question
+     * @return string Returns the flag hint button if it should be displayed else will return blank spacer
+     */
+    protected function flagHintButton($prim){
+        if($this->review != 'answers'){
+            if($this->questionFlagged($prim)){$flagged = ' flagged';}
+            return '<div class="flag'.$flagged.' btn btn-theory"><span class="fa fa-flag fa-fw"></span><span class="hidden-xs"> Flag Question</span></div>';
+        }
+        return '<div class="blank"></div>';
+    }
+    
+    /**
+     * Marks the current test
+     * @return void nothing is returned
+     */
+    protected function markTest(){
+        $this->getQuestions();
+        foreach($this->questions as $prim){
+             if($_SESSION['test'.$this->getTest()][$this->questionNo($prim)]['status'] == 4){$type = 'correct';}
+             else{$type = 'incorrect';}
+             
+             $dsa = $this->getDSACat($prim);
+             $this->testresults['dsa'][$dsa][$type] = (int)$this->testresults['dsa'][$dsa][$type] + 1;
+        }
+        
+        $pass = true;
+        foreach($this->testresults['dsa'] as $category => $value){
+            if($pass != false){
+                if($value['correct'] < $this->passmarkPerCat){$pass = false;}
+            }
+        }
+        
+        $this->testresults['correct'] = $this->numCorrect();
+        $this->testresults['incorrect'] = ($this->numQuestions() - $this->numCorrect());
+        $this->testresults['incomplete'] = $this->numIncomplete();
+        $this->testresults['flagged'] = $this->numFlagged();
+        $this->testresults['numquestions'] = $this->numQuestions();
+        $this->testresults['percent']['correct'] = round(($this->testresults['correct'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['incorrect'] = round(($this->testresults['incorrect'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['flagged'] = round(($this->testresults['flagged'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['incomplete'] = round(($this->testresults['incomplete'] / $this->testresults['numquestions']) * 100);
+        if($this->numCorrect() >= $this->passmark && $pass == true){
+            $this->testresults['status'] = 'pass';
+            $status = 1;
+        }
+        else{
+            $this->testresults['status'] = 'fail';
+            $status = 2;
+        }
+        self::$db->update($this->progressTable, array('status' => $status, 'results' => serialize($this->testresults), 'complete' => date('Y-m-d H:i:s'), 'totalscore' => $this->numCorrect()), array('user_id' => $this->getUserID(), 'id' => $this->getTest(), 'status' => 0, 'type' => $this->getTestType()));
+    }
+    
+    /**
+     * Produces the amount of time the user has spent on the test
+     * @param int $time This should be the amount of seconds remaining for the current test
+     */
+    public function setTime($time, $type = 'taken'){
+        if($time){
+            if($type == 'taken'){
+                list($hours, $mins, $secs) = explode(':', $time);
+                $time = gmdate('H:i:s',($this->seconds - (($hours * 60 * 60) + ($mins * 60) + $secs)));
+                self::$db->update($this->progressTable, array('time_'.$type => $time), array('user_id' => $this->getUserID(), 'id' => $this->getTest(), 'status' => 0, 'type' => $this->getTestType()));
+            }
+            else{
+                $_SESSION['time_'.$type]['test'.$this->getTest()] = $time;
+            }
+        }
+    }
+    
+    /**
+     * Returns the number of seconds remaining for the current test
+     * @return int Returns the number of seconds remaining for the current test
+     */
+    protected function getSeconds(){
+        $time = $this->getTime('remaining');
+        list($hours, $minutes, $seconds) = explode(':', $time);
+        return (($hours * 3600) + ($minutes * 60) + $seconds);
+    }
+    
+    /**
+     * Returns the print certificate/results button to display on the page
+     * @return string Returns either the print certificate of results button depending on how the user did on the test
+     */
+    protected function printCertif(){
+        return '<div class="blank"></div>';
+    }
+    
+    /**
+     * Returns the test results for the current test
+     * @return boolean|array If the test has been completed the test results will be returned as an array else will return false
+     */
+    public function getTestResults(){
+        $testID = $this->getTest();
+        $results = self::$db->select($this->progressTable, array('user_id' => $this->getUserID(), 'id' => $testID, 'type' => $this->getTestType(), 'status' => array('>', 0)), array('id', 'test_id', 'results', 'started', 'complete', 'time_taken', 'status'), array('started' => 'DESC'));
+        if($results){
+            $this->testresults = unserialize($results['results']);
+            $this->testresults['id'] = $results['id'];
+            $this->testresults['test_id'] = $results['test_id'];
+            $this->testresults['complete'] = $results['complete'];
+            return $this->testresults;
+        }
+        return false;
+    }
+    
+    /**
+     * Gets the Time taken or 'remaining for the current test
+     * @param string $type This should be either set to 'taken' or 'remaining' depending on which you wish to get 'taken' by default
+     * @return string Returns the time from the database
+     */
+    public function getTime($type = 'taken'){
+        return self::$db->fetchColumn($this->progressTable, array('user_id' => $this->getUserID(), 'id' => $this->getTest(), 'type' => $this->getTestType()), array('time_'.$type), 0, array('started' => 'DESC'));
+    }
 }
