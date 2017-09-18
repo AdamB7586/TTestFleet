2c96276a8857ef19f4f4d0ee2e043d748b8b00ec
Adam Binnersley <abinnersley@gmail.com>
2017-09-18 15:26:29 +0100

    Update certificate to generate fleet data

--- src/TheoryTestCertificate.php
+++ src/TheoryTestCertificate.php
@@ -3,5 +3,116 @@
 namespace TheoryTest\Fleet;
 
 class TheoryTestCertificate extends \TheoryTest\Car\TheoryTestCertificate{
+    protected $testType = 'FLEET';
     
+    public function generateCertificate(){
+        $this->theory->getQuestions();
+        $this->theory->getTestResults();
+        $this->theory->getUserAnswers();
+        $userInfo = self::$user->getUserInfo();
+        if(!$this->theory->testresults['status']){redirect('/tests/theory.htm');}
+        
+        $this->PDFInfo();
+        if($this->theory->testresults['status'] == 'pass'){
+            $this->pdf->AddPage();
+            $this->pdf->Image('images/cert.jpg', 0, 0, 210, 297);
+            $this->pdf->SetFont('Arial','B', 24);
+            $this->pdf->Ln(30);
+            $this->pdf->Cell(190, 15, strip_tags($this->theory->getTestName()), 0, 0, 'C');
+            $this->pdf->Ln(30);
+            $this->pdf->SetFont('Arial','B', 18);
+            $this->pdf->Cell(10, 14, '', 0); $this->pdf->Cell(28, 14, 'Candidate', 0);
+            $this->pdf->Ln(12);
+            $this->certLine('Name:', $userInfo['first_name'].' '.$userInfo['last_name']);
+            $this->pdf->Ln(10);
+            $this->pdf->SetFont('Arial','B', 18);
+            $this->pdf->Cell(10, 10, '', 0); $this->pdf->Cell(14, 10, 'Test', 0);
+            $this->pdf->Ln(12);
+            $this->certLine('Test ID:', strtoupper($this->testType).$this->theory->testresults['id']);
+            $this->certLine('Test Name:', strip_tags($this->theory->getTestName()));
+            $this->certLine('Completion Date/Time:', date('d/m/Y g:i A', strtotime($this->theory->testresults['complete'])));
+            $this->certLine('Score:', $this->theory->testresults['correct'].' / '.$this->theory->numQuestions());
+            $this->certLine('Passmark:', $this->theory->passmark.' / '.$this->theory->numQuestions());
+            $this->pdf->SetFont('Arial','B', 14);
+            $this->pdf->Cell(10, 10, '', 0); $this->pdf->Cell(72, 10, 'Status:', 0);
+            $this->pdf->SetTextColor(0,151,0);
+            $this->pdf->Cell(92, 10, 'Passed', 0);
+            $this->pdf->SetTextColor(0,0,0);
+        }
+        
+        $this->pdf->AddPage('P', 'A4');
+        $this->pdf->SetFont('Arial','B', 8);
+        $detailsheader = array('Name', 'Test Name', 'Unique Test ID', 'Taken on Date/Time');
+        $details = array(array($userInfo['first_name'].' '.$userInfo['last_name'], strip_tags($this->theory->getTestName()), $this->theory->testresults['id'], date('d/m/Y g:i A', strtotime($this->theory->testresults['complete']))));
+        $tablewidths = array(52,52,39,47);
+        $this->pdf->basicTable($detailsheader, $details, $tablewidths);
+        $this->pdf->Ln();
+        $this->pdf->SetFont('Arial','B', 16);
+        $this->pdf->Cell(92, 10, 'Theory Test Report', 0);
+        $this->pdf->Ln(8);
+        $this->pdf->SetFont('Arial','', 12);
+        if($this->theory->testresults['status'] == 'pass'){
+            $this->pdf->Cell(184, 10, "Congratulations ".$userInfo['first_name']); $this->pdf->Ln(4);
+            $this->pdf->Cell(184, 10, "You have passed this test with ".$this->theory->testresults['percent']['correct']."%."); $this->pdf->Ln(4);
+            $this->pdf->Cell(184, 10, "You answered ".$this->theory->testresults['correct']." out of ".$this->theory->testresults['numquestions']." questions correctly");
+        }
+        else{
+            $this->pdf->Cell(184, 10, "Sorry ".$userInfo['first_name'].", but you have not passed this time."); $this->pdf->Ln(4);
+            $this->pdf->Cell(184, 10, "You answered ".$this->theory->testresults['correct'].' out of '.$this->theory->testresults['numquestions']." questions correctly, the pass rate is ".$this->theory->passmark." out of ".$this->theory->testresults['numquestions']);
+        }
+        $this->pdf->Ln(12);
+        $this->infoLine('Test:', strip_tags($this->theory->getTestName()));
+        $this->infoLine('Date:', date('d/m/Y', strtotime($this->theory->testresults['complete'])));
+        $this->pdf->Ln(6);
+        $this->infoLine('Status:', strip_tags($this->theory->testStatus()));
+        $this->infoLine('Questions:', $this->theory->testresults['numquestions']);
+        $this->pdf->Ln(6);
+        if($this->testType != 'free'){$this->infoLine('Candidate:', $userInfo['first_name'].' '.$userInfo['last_name']);}
+        $this->infoLine('Time Taken:', $this->theory->getTime());
+        $this->pdf->Ln(16);
+        $this->pdf->SetFont('Arial','B', 8);
+
+        $this->overallResults();
+    }
+    
+    private function overallResults(){
+        $header = array('Group', 'Topics in group', 'Correct', 'Incorrect', 'Total', 'Percentage', 'Status');
+        foreach(self::$db->selectAll($this->theory->dsaCategoriesTable) as $group => $data){
+            $correct = (int)$this->theory->testresults['dsa'][$data['section']]['correct'];
+            $incorrect = (int)$this->theory->testresults['dsa'][$data['section']]['incorrect'];
+            $total = $correct + $incorrect;
+
+            if($correct >= $this->theory->passmarkPerCat){$groupstatus = 'Passed';}
+            else{$groupstatus = 'Failed';}
+            $groupdata[] = array($data['section'], substr($data['name'], 0, 53), $correct, $incorrect, $total, number_format((($correct / $total) * 100), 0).'%', $groupstatus);
+            
+            $totalcorrect = $totalcorrect + $correct;
+            $totalincorrect = $totalincorrect + $incorrect;
+            $totalq = $totalq + $total;
+        }
+        $widths = array(14,78,19,19,19,20,21);
+        $this->pdf->basicTable($header, $groupdata, $widths, 6, 2);
+        $first = true;
+        $grouppercent = round(($totalcorrect / $totalq) * 100);
+        
+        if($this->theory->testresults['status'] == 'pass'){$status = 'Passed';}
+        else{$status = 'Failed';}
+        
+        $overall = array('', 'Overall Status', $totalcorrect, $totalincorrect, $totalq, $grouppercent.'%', $status);
+        $this->pdf->SetFont('Arial','B', 9);
+        foreach($widths as $col){
+            if($first == true){$first = false; $currentvalue = current($overall);}else{$currentvalue = next($overall);}
+            $this->pdf->Cell($col, 6, $currentvalue, 1, 0, 'C');
+        }
+        
+        $this->pdf->AddPage('P', 'A4');
+        $this->pdf->SetFont('Arial','B', 9);
+        $testheader = array('Question', 'Learning Section', 'Question No.', 'Status');
+        foreach($this->theory->questions as $question => $prim){
+            if(self::$useranswers[$question]['status'] == '4'){$correct = 'Correct';}else{$correct = 'Incorrect';}
+            $questioninfo = $this->theory->questionInfo($prim);
+            $testdata[] = array($question, $questioninfo['cat'], $questioninfo['topic'], $correct);
+        }
+        $this->pdf->basicTable($testheader, $testdata, array(22,98,30,40), 5, 2);
+    }
 }
